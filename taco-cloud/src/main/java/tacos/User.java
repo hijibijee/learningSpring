5f7d803225958b770aaa1331917f3ade0a332e47
hijibijee <tashrifahmedtaif07@gmail.com>
2023-05-08 15:17:30 +0600

    fix : GenerationTarget encountered exception accepting command : Error executing DDL "drop table if exists user CASCADE " via JDBC Statement

--- taco-cloud/src/main/java/tacos/User.java
+++ taco-cloud/src/main/java/tacos/User.java
@@ -8,14 +8,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Arrays;
 import java.util.Collection;
 
 @Entity
+@Table(name = "\"user\"")
 @Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @RequiredArgsConstructor
