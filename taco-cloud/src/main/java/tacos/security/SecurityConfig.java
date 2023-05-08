9845145817569a5f304720f66608bb86752c8bb7
hijibijee <tashrifahmedtaif07@gmail.com>
2023-05-08 12:28:49 +0600

    add custom user details service bean

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -2,8 +2,12 @@ package tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import tacos.User;
+import tacos.UserDetailsService;
+import tacos.data.UserRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +15,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null) return user;
+
+            throw new UsernameNotFoundException("User: " + username + " not found");
+        };
     }
 }
