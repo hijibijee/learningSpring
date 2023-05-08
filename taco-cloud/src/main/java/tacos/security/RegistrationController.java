17c1341f23d5a4382e008c3f2eab0ee741d2a4b8
hijibijee <tashrifahmedtaif07@gmail.com>
2023-05-08 21:12:02 +0600

    customize login

--- taco-cloud/src/main/java/tacos/security/RegistrationController.java
+++ taco-cloud/src/main/java/tacos/security/RegistrationController.java
@@ -1,14 +1,17 @@
 package tacos.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.User;
 import tacos.data.UserRepository;
 
 @Controller
 @RequestMapping("/register")
+@Slf4j
 public class RegistrationController {
     private UserRepository userRepo;
     private PasswordEncoder passwordEncoder;
@@ -28,7 +31,9 @@ public class RegistrationController {
 
     @PostMapping
     public String processRegistration(RegistrationForm form) {
-        userRepo.save(form.toUser(passwordEncoder));
+        User user = form.toUser(passwordEncoder);
+        log.info("Saving user: {}", user);
+        userRepo.save(user);
         return "redirect:/login";
     }
 }
--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,14 +1,18 @@
 package tacos.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import tacos.User;
 import tacos.data.UserRepository;
 
 @Configuration
+@Slf4j
 public class SecurityConfig {
 
     @Bean
@@ -20,9 +24,26 @@ public class SecurityConfig {
     UserDetailsService userDetailsService(UserRepository userRepo) {
         return username -> {
             User user = userRepo.findByUsername(username);
+            log.info("User: {}", user);
             if (user != null) return user;
 
             throw new UsernameNotFoundException("User: " + username + " not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeRequests()
+                .antMatchers("/design", "/orders").access("hasRole('USER')")
+                .antMatchers("/", "/**").access("permitAll()")
+
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+
+            .and()
+                .build();
     }
 }
